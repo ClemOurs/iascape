b1038ed2be022b07274f4912f16e8f0c77baf4d6
Clément <duvivier.clem@gmail.com>
2022-11-17 18:20:32 +0100

    :white_check_mark: dev enigma done

---
+++ pages/game/enigmas/dev.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="enigma">
+        <TheEnigma 
+            title="Oops, l’attaque s’est propagé jusqu’au code source de l’école"
+            difficulty="facile"
+            time="5"
+            text="La maléfique Medusa s’est amusé à ennuyer les élèves de l’axe Création & Design en s’attaquant aux projets et au travaux des étudiants. 
+                Elle a dissimulé des indices en espérant laisser une chance aux plus malins de réussir à déjouer ses tours.
+                Les énigmes qui suivent vous permettront d’avoir un aperçu de cet axe et d’enrichir (peut-être) votre culture artistique et d’éveiller l’âme de créateur qui sommeille en vousMedusa s’est introduite dans le code source de l’école et continue à ennuyer les étudiants de l’IIM. 
+                Un virus a infecté les machines et les étudiants en coding se sont portés volontaires pour arrêter l’attaque.
+                Dans les énigmes suivantes, vous allez aider les étudiants en Coding & Digital Innovation à faire façon aux pièges tendus par Medusa."
+        />
+        <img id="illu" src="~/assets/illustrations/illustration_enigma_2.jpg"  alt="image d'illustration énigme"/>
+    </div>
+</template>
+<script>
+export default {
+    name: "DevGamePage",
+    layout: "blank",
+}
+</script>
